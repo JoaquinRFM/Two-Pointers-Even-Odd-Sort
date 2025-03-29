75867ad6cf7b76144f909694104f010bc18bed4f
JoaquinRFM <joaquin123.flomat@gmail.com>
2025-03-29 09:31:45 -0500

    Basic Code to Sort

---
+++ Two Pointers Even-Odd Sort.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void merge(int* p, int* q) {}
+
+int main() {      //p             q
+    int arr[10] = { 2, 4, 8, 10, 12, 1, 3, 5, 7, 9 };
+    int* p;
+    int* q;
+    merge(p, q);
+}
