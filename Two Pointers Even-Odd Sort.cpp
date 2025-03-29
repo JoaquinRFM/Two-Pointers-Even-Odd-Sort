d3a1d52eaac73d568e5f86af6a3ab9b9defedf3e
JoaquinRFM <joaquin123.flomat@gmail.com>
2025-03-29 17:54:48 -0500

    Algoritmo Implementado

--- Two Pointers Even-Odd Sort.cpp
+++ Two Pointers Even-Odd Sort.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
 using namespace std;
 
-void merge(int* p, int* q) {}
+void print(int* arr) {
+    for (int i = 0; i < 10; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
 
-int main() {      //p             q
-    int arr[10] = { 2, 4, 8, 10, 12, 1, 3, 5, 7, 9 };
-    int* p;
-    int* q;
-    merge(p, q);
+void dualPointerSort(int* p, int* q) {
+    int* FIN = (p + ((q - p) * 2)) - 1;
+    for (; p < FIN; p++) {
+        if (p == q) {
+            q++;
+        }
+        int* minor = q;
+        for (int* i = q; i <= FIN; i++) {
+            if (*minor > *i) {
+                minor = i;
+            }
+        }
+        if (*minor < *p) {
+            int temp = *p;
+            *p = *minor;
+            *minor = temp;
+        }
+    }
+}
+
+int main() {
+    int arr[10] = { 2, 4, 6, 8, 10, 1, 3, 5, 7, 9 };
+    cout << "Array: ";
+    print(arr);
+    int* p = arr;
+    int* q = arr + 5;
+    dualPointerSort(p, q);
+    cout << "Sorted array: ";
+    print(arr);
 }
